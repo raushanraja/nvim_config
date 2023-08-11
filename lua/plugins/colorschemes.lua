13c1ad8acb69f9aaf17354db66d802252a3531d6
raushanraja <vraushanraja@gmail.com>
2023-08-12 02:18:20 +0530

    add colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,45 @@
+return {
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+    },
+
+    {
+        'AlexvZyl/nordic.nvim',
+        lazy = false,
+        priority = 1000,
+    },
+
+    {
+        'rose-pine/neovim',
+        lazy = false,
+        name = 'rose-pine',
+        priority = 1000,
+    },
+
+    {
+        'shaunsingh/nord.nvim',
+        lazy = false,
+        name = 'nord',
+        priority = 1000,
+    },
+
+    {
+        'rmehri01/onenord.nvim',
+        lazy = false,
+        name = 'onenord',
+        priority = 1000,
+    },
+}
