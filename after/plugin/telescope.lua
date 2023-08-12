8cafa3aaac1cbda0a5ed89fe25bc65a61894a379
raushanraja <vraushanraja@gmail.com>
2023-08-12 16:37:08 +0530

    telescope plugin

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,9 @@
+-- Add j More picker docs
+-- https://github.com/nvim-telescope/telescope.nvim#pickers
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fr', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
---
+++ lua/plugins/telecsope.lua
@@ -0,0 +1,5 @@
+-- plugins/telescope.lua:
+return {
+	'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	dependencies = { 'nvim-lua/plenary.nvim' }
+}
