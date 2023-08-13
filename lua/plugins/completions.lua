0e4312e4d0c6b09305e7561e662710b138d42f85
raushanraja <vraushanraja@gmail.com>
2023-08-13 19:41:29 +0530

    add completions plugins
    - Add basic cmp plugin
    - Add copilot

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,8 @@
+return {
+    { 'hrsh7th/cmp-nvim-lsp' },                                                        -- Required
+    { "hrsh7th/cmp-nvim-lua" },
+    { 'hrsh7th/cmp-buffer' },                                                          -- Required
+    { 'L3MON4D3/LuaSnip',        dependencies = "rafamadriz/friendly-snippets", },     -- Required
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-path" },
+}
