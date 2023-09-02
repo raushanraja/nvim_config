d47e9579298c6ce48fe98a7c99d2fff77b960809
raushanraja <vraushanraja@gmail.com>
2023-09-02 17:40:56 +0530

    update config

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,4 +1,32 @@
 function Colors(color)
+    require("tokyonight").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true,     -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+            -- Style to be applied to different syntax groups
+            -- Value is any valid attr-list value for `:help nvim_set_hl`
+            comments = { italic = true },
+            keywords = { italic = false },
+            variables = {},
+            functions = { italic = false, bold = true },
+            -- Background styles. Can be "dark", "transparent" or "normal"
+        },
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        -- on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        -- on_highlights = function(highlights, colors) end,
+    })
+
     -- color = color or "onenord"
     -- color = color or "catppuccin-mocha"
     color = color or "tokyonight"
