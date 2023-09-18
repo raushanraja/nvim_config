beccc0cb174e9e74c4ed3770d11d26bf25ddc99f
raushanraja <vraushanraja@gmail.com>
2023-09-18 14:38:37 +0530

    add: autoflake for python import fix

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,11 +1,26 @@
 local util = require("formatter.util")
 local python_formatter = require("formatter.filetypes.python").autopep8
 local js_formatter = require("config.formatters").prettierjs
+local util = require "formatter.util"
+
+-- Create a wrapper function that combines python_formatter with autoflake
+local function autoflake()
+    return {
+        exe = "autoflake",
+        args = {
+            "--in-place",
+            "--remove-all-unused-imports",
+        },
+        stdin = false,
+    }
+end
+
+
 
 
 require("formatter").setup({
     filetype = {
-        python = { python_formatter },
+        python = { autoflake, python_formatter },
         javascript = { js_formatter },
         javascriptreact = { js_formatter }
     }
