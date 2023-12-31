95b9b54ee1f4b6bec530a1deb1b420a83289cdc1
raushanraja <vraushanraja@gmail.com>
2023-12-31 21:08:06 +0530

    some colorscheme updates

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -30,25 +30,25 @@ function Colors(color)
         disable_italics = true,
     })
 
-    -- Set colorscheme after options
-    -- vim.cmd('colorscheme rose-pine')
-
-
-
-    -- color = color or "ayu"
+    color = color or "ayu"
     -- color = color or "onenord"
     -- color = color or "catppuccin-mocha"
     -- color = color or "caret"
-    -- color = color or "tokyonight"
+    -- color = color or "tokyonight-day"
     -- color = color or "rose-pine"
     -- color = color or "catppuccin-latte"
-    color = color or "github_dark_tritanopia"
+    -- color = color or "github_dark_tritanopia"
     -- vim.o.background = "light"
+
+    -- Set colorscheme after options
     vim.cmd.colorscheme(color)
 
+    -- transparent background
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+
     -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
     -- vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
     -- vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
